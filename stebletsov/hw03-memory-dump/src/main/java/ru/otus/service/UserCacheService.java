4f2bf894d872ddbfaccac5b74a283b81e7e25b15
Viacheslav Stebletsov <vstebletsov89@github.com>
2025-01-06 20:11:02 +0100

    fix memory leak

--- stebletsov/hw03-memory-dump/src/main/java/ru/otus/service/UserCacheService.java
+++ stebletsov/hw03-memory-dump/src/main/java/ru/otus/service/UserCacheService.java
@@ -6,18 +6,22 @@ import org.springframework.stereotype.Service;
 import ru.otus.entity.User;
 import ru.otus.repository.UserRepository;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.lang.ref.WeakReference;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserCacheService {
     private final UserRepository userRepository;
-    private static final Map<Long, User> userCache = new ConcurrentHashMap<>();
+    private static final ConcurrentSkipListMap<Long, WeakReference<User>> userCache =
+            new ConcurrentSkipListMap<>();
+    private static final int threshold = 20;
 
     public User getUser(Long id) {
-        var user = userCache.get(id);
+        var ref = userCache.get(id);
+        User user = (ref != null) ? ref.get() : null;
         if (user == null) {
             user = userRepository.findById(id).orElse(null);
         }
@@ -25,10 +29,17 @@ public class UserCacheService {
     }
 
     public void putUser(User user) {
+        log.info("Cache size: {}", userCache.size());
         if (user.getId() == null) {
             throw new RuntimeException("User.id must not be null");
         }
-        userCache.put(user.getId(), user);
+        if (userCache.size() > threshold) {
+            // remove half elements
+            IntStream.range(0, threshold/2)
+                    .takeWhile(i -> !userCache.isEmpty())
+                    .forEach(i -> userCache.pollFirstEntry());
+        }
+        userCache.put(user.getId(), new WeakReference<>(user));
     }
 
     public void evictUser(Long id) {
