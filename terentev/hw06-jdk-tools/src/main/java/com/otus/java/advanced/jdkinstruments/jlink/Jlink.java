d7b105d7a33e470a5b5c5412a03f35893f9649c0
Sergei Terentev <s.terentev@iteco-inno.ru>
2025-01-21 19:37:20 +0300

    Add hw06

---
+++ terentev/hw06-jdk-tools/src/main/java/com/otus/java/advanced/jdkinstruments/jlink/Jlink.java
@@ -0,0 +1,11 @@
+package com.otus.java.advanced.jdkinstruments.jlink;
+
+import java.util.logging.Logger;
+
+public class Jlink {
+
+    private static final Logger LOG = Logger.getLogger(Jlink.class.getName());
+    public static void main(String[] args) {
+        LOG.info("Homework06");
+    }
+}
