57309e0c61464677a100383591ec277bf9095635
Ilya Posypkin <Ilya.Posypkin@rosbank.ru>
2025-01-24 18:21:20 +0300

    fix OutOfMemory

--- posypkin/hw03-memory-dump-homework/src/main/java/ru/otus/memory_dump_homework/service/RegistrationServiceImpl.java
+++ posypkin/hw03-memory-dump-homework/src/main/java/ru/otus/memory_dump_homework/service/RegistrationServiceImpl.java
@@ -7,20 +7,16 @@ import ru.otus.memory_dump_homework.entity.User;
 import ru.otus.memory_dump_homework.model.UserDto;
 import ru.otus.memory_dump_homework.repository.UserRepository;
 
-import java.util.LinkedList;
-
 @Service
 @RequiredArgsConstructor
 public class RegistrationServiceImpl implements RegistrationService {
 
     private final UserRepository repository;
     private final ModelMapper mapper;
-    private final LinkedList<UserDto> userList = new LinkedList<>();
 
     @Override
     public String register(UserDto user) {
-        userList.add(user);
-        return repository.save(mapper.map(userList.getLast(), User.class))
+        return repository.save(mapper.map(user, User.class))
                 .getId();
     }
 
