1d9c2f18d291805a45aca1e870e0299bdcd88086
Sergei Terentev <s.terentev@iteco-inno.ru>
2025-01-27 21:06:02 +0300

    Add hw05

---
+++ terentev/hw05-modules/api/module-info.java
@@ -0,0 +1,8 @@
+module ru.otus.api {
+    requires ru.otus.service;
+    requires ru.otus.core;
+    requires ru.otus.provider;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.web;
+}
