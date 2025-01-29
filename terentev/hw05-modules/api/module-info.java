b2f9abe9356a5ecfbf9873237b54cc72599ceec4
Sergei Terentev <s.terentev@iteco-inno.ru>
2025-01-29 20:29:05 +0300

    Fix after review

--- terentev/hw05-modules/api/module-info.java
+++ terentev/hw05-modules/api/module-info.java
@@ -1,7 +1,6 @@
 module ru.otus.api {
     requires ru.otus.service;
     requires ru.otus.core;
-    requires ru.otus.provider;
     requires spring.boot;
     requires spring.boot.autoconfigure;
     requires spring.web;
