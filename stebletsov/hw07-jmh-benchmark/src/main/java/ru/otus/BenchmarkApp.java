6fcf29013878867855afd6e5ee00fb7634b2928b
Viacheslav Stebletsov <vstebletsov89@github.com>
2025-02-02 15:56:23 +0100

    add hw07

---
+++ stebletsov/hw07-jmh-benchmark/src/main/java/ru/otus/BenchmarkApp.java
@@ -0,0 +1,15 @@
+package ru.otus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableCaching
+@EnableScheduling
+public class BenchmarkApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BenchmarkApp.class, args);
+    }
+}
