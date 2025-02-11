87074ce124628e13132c06b316af0283611eac30
Denis Gusarov <Gusarov@seaexpress.ru>
2025-02-11 18:23:59 +0300

    описание железа. Вынес в переменную пароль.

--- gusarov/benchmark/src/main/java/org/example/task7/jmh/HashBenchmarkTest.java
+++ gusarov/benchmark/src/main/java/org/example/task7/jmh/HashBenchmarkTest.java
@@ -4,35 +4,28 @@ import org.example.task7.hashutil.PasswordHash;
 import org.openjdk.jmh.annotations.*;
 import java.util.concurrent.TimeUnit;
 
-@BenchmarkMode({Mode.Throughput, Mode.AverageTime, Mode.SampleTime, Mode.SingleShotTime})
+@BenchmarkMode(Mode.All)
+@Fork(value = 1, warmups = 0)
+@Warmup(iterations = 10, time = 5, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 10, time = 5, timeUnit = TimeUnit.MILLISECONDS)
 @State(Scope.Thread)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class HashBenchmarkTest {
 
+    private String password = "Password";
+
     @Benchmark
-    @BenchmarkMode(Mode.All)
-    @Fork(value = 1, warmups = 0)
-    @Warmup(iterations = 10, time = 5, timeUnit = TimeUnit.MILLISECONDS)
-    @Measurement(iterations = 10, time = 5, timeUnit = TimeUnit.MILLISECONDS)
     public byte[] md5Hash() {
-        return PasswordHash.createPasswordHash("Password", "MD5");
+        return PasswordHash.createPasswordHash(password, "MD5");
     }
 
     @Benchmark
-    @BenchmarkMode(Mode.All)
-    @Fork(value = 1, warmups = 0)
-    @Warmup(iterations = 10, time = 5, timeUnit = TimeUnit.MILLISECONDS)
-    @Measurement(iterations = 10, time = 5, timeUnit = TimeUnit.MILLISECONDS)
     public byte[] sha256Hash() {
-        return PasswordHash.createPasswordHash("Password", "SHA-256");
+        return PasswordHash.createPasswordHash(password, "SHA-256");
     }
 
     @Benchmark
-    @BenchmarkMode(Mode.All)
-    @Fork(value = 1, warmups = 0)
-    @Warmup(iterations = 10, time = 5, timeUnit = TimeUnit.MILLISECONDS)
-    @Measurement(iterations = 10, time = 5, timeUnit = TimeUnit.MILLISECONDS)
     public byte[] sha512Hash() {
-        return PasswordHash.createPasswordHash("Password", "SHA-512");
+        return PasswordHash.createPasswordHash(password, "SHA-512");
     }
 }
