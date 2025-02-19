7b4c652304c03ba1ab13fd7680e8be4e60327fbe
Gusarov Denis <gusarov.denis.y@yandex.ru>
2025-02-19 18:46:56 +0300

    visualvm_asyncprofiler

---
+++ gusarov/visualvmap/src/main/java/org/example/task10/validation/JsonValidationException.java
@@ -0,0 +1,7 @@
+package org.example.task10.validation;
+
+public class JsonValidationException extends RuntimeException {
+    public JsonValidationException(String message) {
+        super(message);
+    }
+}
