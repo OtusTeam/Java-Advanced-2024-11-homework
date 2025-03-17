89e6f24cadab5a52afa3b482e587632fb4b9f0d6
vs891026 <vyacheslav.stebletsov@broadcom.com>
2025-03-17 10:06:34 +0100

    code review comments

--- stebletsov/hw12-spring-web-flux/src/main/java/ru/otus/ReactiveApp.java
+++ stebletsov/hw12-spring-web-flux/src/main/java/ru/otus/ReactiveApp.java
@@ -2,9 +2,6 @@ package ru.otus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class ReactiveApp {
